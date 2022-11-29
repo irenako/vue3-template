a1a692761a2faf6ebeae60b389c04f2aa99ff06a
irenako <108225581+irenako@users.noreply.github.com>
2022-11-29 16:11:44 +0200

    Created vite template

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import md from "./  vite-plugin-vue-md.js";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    md({
+      wrapperElement: "section",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
